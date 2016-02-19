84afc10d70c05309fa938e0f60973ad3762a9811
etang <etang315@gmail.com>
2016-02-19 16:34:57 -0600

    added a if else condition to take the value from the input field. To check if the user enter an email or not. HTML has embbed pattern to check if there no javascript enable on html webpage.

--- project-1-protype/form.js
+++ project-1-protype/form.js
@@ -2,9 +2,15 @@ var button1 = document.getElementById("submit");
 
 button1.addEventListener('click',function subscribe(){
     
-  
-    document.getElementById("thank_you").innerHTML=("Thank you for subscribing to Golden Aquarium!");
+    var x = document.forms["my_form"]["email"].value;
     
+    if (x==null || x==""){
+    document.getElementById("thank_you").innerHTML=("Please enter an email!");
+    }
+    
+    else{
+         document.getElementById("thank_you").innerHTML=("Thank you for subscribing to Golden Aquarium!");
+    }
 
     
     
